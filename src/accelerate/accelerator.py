ecc0a0c9e610222b95e9a664e4ef2fefa1494213
Mark-ZhouWX <zhouwuxing000@gmail.com>
2026-02-10 14:48:30 +0800

    fix bug of hsdp naming

--- src/accelerate/accelerator.py
+++ src/accelerate/accelerator.py
@@ -90,7 +90,7 @@ from .utils import (
     fsdp2_prepare_model,
     hsdp_prepare_model,
     fsdp2_switch_optimizer_parameters,
-    hsdp2_switch_optimizer_parameters,
+    hsdp_switch_optimizer_parameters,
     gather,
     gather_object,
     get_fsdp2_grad_scaler,
@@ -1812,7 +1812,7 @@ class Accelerator:
         # Update the optimizer parameters
         for obj in result:
             if isinstance(obj, torch.optim.Optimizer):
-                hsdp2_switch_optimizer_parameters(obj, mapping)
+                hsdp_switch_optimizer_parameters(obj, mapping)
 
         return result
 
